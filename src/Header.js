5091c73f1ecc922caac9714f9acac561dafdd6bc
IvanR <ivan.ramirezf@gmail.com>
2024-10-29 22:18:52 +1000

    Inline CSS

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+
+  return <h1 style={style}>Fast React Pizza Co.</h1>;
 }
 
 export default Header;
