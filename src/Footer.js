27cbacd0e058428eefd07d3b0c2f2997fc6416b8
IvanR <ivan.ramirezf@gmail.com>
2024-11-01 23:54:36 +1000

    Conditional rendering: Ternary operator

--- src/Footer.js
+++ src/Footer.js
@@ -2,20 +2,24 @@ import React from "react";
 
 function Footer() {
   const openHour = 12;
-  const closeHour = 24;
+  const closeHour = 23;
   const hour = new Date().getHours();
 
   const isOpen = hour >= openHour && hour < closeHour;
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>
             We are open until {closeHour}:00. Come visit us or order online.
           </p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We are happy to welcome you between {openHour}:00 and {closeHour}:00{" "}
+        </p>
       )}
     </footer>
   );
